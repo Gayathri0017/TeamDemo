d09865e091d7039d49c1ecd3f89dd0e6a0268716
Gayathri0017 <gayathri4866@gmail.com>
2025-02-06 16:40:01 +0530

    Add files via upload

---
+++ demos.java
@@ -0,0 +1,12 @@
+public class demos{
+public static void main(String args[]) {
+if(args.length<2){
+System.out.println("Usage: java parameterInput <name> <age>");
+return;
+}
+String name=args[0];
+int age=Integer.parseInt(args[1]);
+System.out.println("Name:"+name);
+System.out.println("Age:"+age);
+}
+}
