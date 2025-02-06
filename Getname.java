664e41d0fe1f4a4055c62963b5c1fab011f5b5bf
DharaniMurugan01 <2k21dhara@gmail.com>
2025-02-06 16:30:37 +0530

    Add files via upload

---
+++ Getname.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Getname{
+public static void main(String []args){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter name");
+String name=sc.next();
+System.out.println("Enter age");
+int age=sc.nextInt();
+System.out.println("name is"+name+" age  "age);
+}
+}
