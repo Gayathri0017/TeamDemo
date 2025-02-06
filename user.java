a6e2c735c3796fcee08f89369db57f23382ff46d
Gayathri0017 <gayathri4866@gmail.com>
2025-02-06 16:24:04 +0530

    Add files via upload

---
+++ user.java
@@ -0,0 +1,12 @@
+public class Demo{
+public static void main(String args[]) {
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter name");
+String name=sc.next();
+System.out.println("Enter your age:");
+int age=sc.nextInt();
+System.out.println("Name:"+name);
+System.out.println("Age:"+age);
+}
+}
+
