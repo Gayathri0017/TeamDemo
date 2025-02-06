245ae41c8ae00af2389eb3c2ba23cae62b13c66a
Gayathri0017 <gayathri4866@gmail.com>
2025-02-06 16:37:42 +0530

    user.java

--- user.java
+++ user.java
@@ -1,10 +1,11 @@
 public class user{
 public static void main(String args[]) {
-Scanner sc=new Scanner(System.in);
-System.out.println("Enter name");
-String name=sc.next();
-System.out.println("Enter your age:");
-int age=sc.nextInt();
+if(args.length<2){
+System.out.println("Usage: java parameterInput <name> <age>");
+return;
+}
+String name=args[0];
+int age=Integer.parseInt(args[1]);
 System.out.println("Name:"+name);
 System.out.println("Age:"+age);
 }
